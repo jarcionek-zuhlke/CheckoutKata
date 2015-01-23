4bd69b83bf538c1406d0d89d9fd37dfe7b75f6a6
Jaroslaw Pawlak <jaroslaw.pawlak@zuhlke.com>
2015-01-23 10:17:13 +0000

    refactoring (more java 8 streams)

--- src/main/java/checkoutkata/PriceCalculator.java
+++ src/main/java/checkoutkata/PriceCalculator.java
@@ -1,11 +1,11 @@
 package checkoutkata;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
 import static java.util.stream.StreamSupport.stream;
 
 public class PriceCalculator {
@@ -17,33 +17,35 @@ public class PriceCalculator {
     }
 
     public BigDecimal calculateTotalPriceFor(Iterable<Item> items, Iterable<Offer> specialOffers) {
-        if (!items.iterator().hasNext()) {
+        if (isEmpty(items)) {
             throw new IllegalArgumentException("no items provided");
         }
 
-        Map<Character, Long> count = stream(items.spliterator(), false).collect(groupingBy(Item::getSku, counting()));
+        Map<Character, Offer> offers = stream(specialOffers.spliterator(), false)
+                .collect(groupingBy(Offer::getItemSku, reducing(null, (a, b) -> b)));
 
-        Map<Character, Offer> offers = new HashMap<>();
-        for (Offer offer : specialOffers) {
-            offers.put(offer.getItemSku(), offer);
-        }
-
-        return count.entrySet().stream()
-                .map(e -> {
-                    char sku = e.getKey();
-                    BigDecimal individualPrice = priceProvider.getPrice(sku);
-                    long itemCount = count.get(sku);
-                    if (offers.containsKey(sku)) {
-                        Offer offer = offers.get(sku);
-                        long multiples = itemCount / offer.getNumberOfItems();
-                        long remainder = itemCount % offer.getNumberOfItems();
+        return stream(items.spliterator(), false)
+                .collect(groupingBy(Item::getSku, counting()))
+                .entrySet().stream()
+                .map(entry -> {
+                    long numberOfItems = entry.getValue();
+                    BigDecimal individualPrice = priceProvider.getPrice(entry.getKey());
+                    Offer offer = offers.get(entry.getKey());
+                    if (offer != null) {
+                        long multiples = numberOfItems / offer.getNumberOfItems();
+                        long remainder = numberOfItems % offer.getNumberOfItems();
                         return offer.getTotalPrice().multiply(BigDecimal.valueOf(multiples))
                                 .add(individualPrice.multiply(BigDecimal.valueOf(remainder)));
                     } else {
-                        return individualPrice.multiply(BigDecimal.valueOf(itemCount));
+                        return individualPrice.multiply(BigDecimal.valueOf(numberOfItems));
                     }
                 })
-                .reduce(BigDecimal::add).get();
+                .reduce(BigDecimal::add)
+                .get();
+    }
+
+    private static boolean isEmpty(Iterable<Item> items) {
+        return !items.iterator().hasNext();
     }
 
 }
